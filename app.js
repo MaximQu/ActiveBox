3a36c35c99e74be9aec4af89196d1a3206162339
Maxim <63422884+MaximQu@users.noreply.github.com>
2022-02-27 21:54:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+$(function () {
+
+    /* Fixed header  */
+    let header = $('#header');
+    let intro = $('#intro');
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $('#nav');
+    let navToggle = $('#navToggle');
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on('scroll resize' , function () {
+        let introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+        function checkScroll(scrollPos, introH) {
+            if( scrollPos > introH ) {
+                header.addClass('fixed');
+            } else {
+                header.removeClass('fixed');
+            };
+        };
+    
+    /* Smooth Scroll */
+
+    $('[data-scroll]').on('click', function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffSet = $(elementId).offset().top;
+
+        nav.removeClass('show');
+
+        $('html,  body').animate({
+            scrollTop: elementOffSet - 60
+        }, 500);
+
+    });
+
+    /* navToggle */
+
+    $('#navToggle').on('click',  function(event) {
+        event.preventDefault();
+        nav.toggleClass('show');
+
+    });
+
+    let slider = $('#reviewsSlider');
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
+});
